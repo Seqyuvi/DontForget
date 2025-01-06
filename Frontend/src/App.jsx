5055502e6f93248812efea052d5e8179a842c808
kingoffier <volozhanin53@mail.ru>
2025-01-06 17:07:11 +0500

    v2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
+import { useEffect } from "react";
 import HelloPage from "./components/HelloPage";
-
+import { fetchNotes } from "./services/note";
 
 export default function App() {
+ 
   return (
     <>
-        <HelloPage/>
+      <HelloPage />
     </>
   );
 }
-
-
--- Frontend/src/components/MainPage.jsx
+++ Frontend/src/components/MainPage.jsx
@@ -4,8 +4,43 @@ import search from "../assets/search.svg";
 import today from "../assets/today.svg";
 import future from "../assets/future.svg";
 import filters from "../assets/filters.svg";
+import React, { useEffect, useState } from "react";
+import CreateNote from "./CreateNote";
+import Note from "./Note";
+import axios from "axios";
 
 export default function MainPage() {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await axios.get(
+          "http://localhost:5300/api/Task/GetAllTask",
+          { withCredentials: true }
+        );
+        setTasks(response.data);
+      } catch (error) {
+        setError(error.message || "An error occurred");
+        console.error("Fetching tasks failed", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading tasks...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching tasks: {error}</p>;
+  }
   return (
     <main className="dontForget">
       <section className="menu">
@@ -37,17 +72,35 @@ export default function MainPage() {
         </ul>
       </section>
       <section className="notes">
-        <div className="note">
-          <p className="date">Сегодня</p>
-          <div className="description">
-            <input placeholder="Название задачи" type="text" />
-            <input placeholder="Описание" type="text" />
-            <div className="create">
-              <button className="cancelbutton">Отмена</button>
-              <button className="addbutton">Добавить задачу</button>
-            </div>
-          </div>
-        </div>
+        <ul className="mama">
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {/* <p>{task.nameTask}</p>
+                  <p>Description: {task.description}</p>
+                  <p>Data:{task.date}</p> */}
+              {/* <p>Due Date: {new Date(task.Date).toLocaleString()}</p> */}
+              {/* <Note
+                title={task.nameTask}
+                description={task.description}
+                data={task.date}
+              /> */}
+              <div className="note">
+                <p className="date">{task.date}</p>
+                <div className="description">
+                  <p className="title">{task.nameTask}</p>
+                  <p className="task">{task.description}</p>
+                  <div className="create">
+                    <button className="cancelbutton">Удалить задачу</button>
+                    <button className="addbutton">Редактировать</button>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+          <li className="asd">
+            <CreateNote />
+          </li>
+        </ul>
       </section>
     </main>
   );
